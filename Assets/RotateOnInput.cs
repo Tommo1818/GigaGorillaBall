16ef509b77a554dbbb119872d4ef99b2dfbf2b4d
Tommo <tvernoncsi@gmail.com>
2024-02-21 21:31:28 +0000

    level rotation now snaps back to level

--- Assets/RotateOnInput.cs
+++ Assets/RotateOnInput.cs
@@ -6,6 +6,14 @@ public class RotateLevelGameObject : MonoBehaviour
 {
     public float rotationSpeed; // Adjust this value for desired rotation speed
     public Transform pivotPoint; // The object around which to rotate
+    public float smoothingSpeed;
+    private float currentRotationLerpValue;
+    public Quaternion initialRotation;
+
+    void Start()
+    {
+        initialRotation = transform.rotation;
+    }
 
     void Update()
     {
@@ -16,9 +24,25 @@ public class RotateLevelGameObject : MonoBehaviour
         else if (Input.GetKey(KeyCode.D))
         {
             RotateAntiClockwise(); // Call function for anticlockwise rotation
+        } 
+        else if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            ResetRotation();
+        }
+        
+    }
+    void ResetRotation()
+    {
+        if (Quaternion.Angle(transform.rotation, initialRotation) > 0)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, initialRotation, currentRotationLerpValue);
+            currentRotationLerpValue += smoothingSpeed * Time.deltaTime;
+        }
+        else
+        {
+            currentRotationLerpValue = 0;
         }
     }
-
     private void RotateClockwise()
     {
         transform.RotateAround(pivotPoint.position, Vector3.forward, rotationSpeed * Time.deltaTime);
