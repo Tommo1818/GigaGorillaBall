b74d0f4b9944ff43eabf09e0d1fa9154fc809ff1
Tommo <tvernoncsi@gmail.com>
2024-02-26 22:54:03 +0000

    jumping now works properly!
    maybe add this as a pickup though

---
+++ Assets/Movement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float jumpForce;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    public LayerMask groundLayers; // Assign the layers considered as ground in the inspector
+    public float extraHeightText = 0.1f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsTouchingGround() && Input.GetKeyDown(KeyCode.Space))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 1f, groundLayers);
+
+            if (hit.collider != null)
+            {
+                Vector2 jumpDirection = hit.normal;
+                rb.AddForce(jumpDirection * jumpForce, ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    bool IsTouchingGround()
+    {
+        Vector2 boxCastSize = new Vector2(transform.localScale.x, extraHeightText);
+        Vector2 boxCastOrigin = (Vector2)transform.position - new Vector2(0, transform.localScale.y / 2);
+
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCastOrigin, boxCastSize, 0f, Vector2.down, extraHeightText, groundLayers);
+        return raycastHit.collider != null;
+    }
+}
